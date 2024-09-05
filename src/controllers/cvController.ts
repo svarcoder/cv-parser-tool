b0ffed9fd69e350f626c0403182e4e9db308c7ba
svarcoder <charvai1997@gmail.com>
2024-09-05 16:36:20 +0530

    perf: add extract fn

--- src/controllers/cvController.ts
+++ src/controllers/cvController.ts
@@ -2,6 +2,7 @@ import fs from "fs";
 import path from "path";
 import handlebars from "handlebars";
 import { Request, Response } from "express";
+import mammoth from "mammoth";
 const toPdf = require("docx-pdf");
 
 export const generateCV = async (req: Request, res: Response) => {
@@ -30,6 +31,45 @@ export const generateCV = async (req: Request, res: Response) => {
     });
   } catch (error: any) {
     console.error("Error generating PDF:", error);
+    return res.status(404).json({
+      status: 404,
+      messege: error?.message,
+    });
+  }
+};
+
+export const extractTextFromCV = async (req: Request, res: Response) => {
+  try {
+    const cvFile = req.file;
+
+    if (!cvFile) {
+      return res.status(400).json({
+        status: 404,
+        messege: "No CV file uploaded.",
+      });
+    }
+
+    const filePath = path.resolve(__dirname, "../uploads", cvFile.originalname);
+
+    fs.writeFileSync(filePath, cvFile.buffer);
+
+    const result = await mammoth.extractRawText({ path: filePath });
+    console.log(result);
+
+    let extractedText = result.value;
+
+    extractedText = extractedText.replace(/\n+/g, " ").trim();
+    extractedText = extractedText.replace(/\n+/g, "\n").trim();
+
+    fs.unlinkSync(filePath);
+
+    return res.status(201).json({
+      status: 201,
+      message: "Text generated successfully",
+      data: extractedText,
+    });
+  } catch (error: any) {
+    console.error("Error extracting text from CV:", error);
     return res.status(404).json({
       status: 404,
       messege: error?.message,
--- src/routes/cvRoutes.ts
+++ src/routes/cvRoutes.ts
@@ -1,8 +1,11 @@
 import express from "express";
-import { generateCV } from "../controllers/cvController";
-
+import { extractTextFromCV, generateCV } from "../controllers/cvController";
+import multer from "multer";
 const router = express.Router();
 
+const upload = multer();
+
 router.post("/generate-cv", generateCV);
+router.post("/extract-text", upload.single("file"), extractTextFromCV);
 
 export default router;
